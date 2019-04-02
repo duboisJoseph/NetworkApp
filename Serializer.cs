a7b9bea56a64f92f39b4eabc23a5e71bf4f1bf22
adnguy3n <32573771+adnguy3n@users.noreply.github.com>
2019-04-02 17:18:22 -0400

    Added some comments.

--- Serializer.cs
+++ Serializer.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-
+/** 
+ * Found this serializer class in 
+ * https://stackoverflow.com/questions/5005900/how-to-serialize-listt.
+ * */
 namespace NetworkApp
 {
     public static class Serializer
     {
+        //Serializes an object using binary serialization and saves it as a .bin file.
         public static void Save(string filePath, object objToSerialize)
         {
             try
@@ -21,6 +25,8 @@ namespace NetworkApp
             }
         }
 
+        //Takes a .bin file and deserializes it into an object. 
+        //Returns the deserialized object.
         public static T Load<T>(string filePath) where T : new()
         {
             T rez = new T();
