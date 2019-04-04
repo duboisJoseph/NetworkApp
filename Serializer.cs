c4f6ef4981f4382cea7944fb0a2f938bd80dcafb
staddlez <yapc@mail.gvsu.edu>
2019-04-03 23:05:49 -0400

    Add files via upload
    https://docs.microsoft.com/en-us/dotnet/framework/network-programming/asynchronous-server-socket-example

---
+++ Serializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+/** 
+ * Found this serializer class in 
+ * https://stackoverflow.com/questions/5005900/how-to-serialize-listt.
+ * */
+namespace NetworkApp
+{
+  public static class Serializer
+  {
+    //Serializes an object using binary serialization and saves it as a .bin file.
+    public static void Save(string filePath, object objToSerialize)
+    {
+      try
+      {
+        using (Stream stream = File.Open(filePath, FileMode.Create))
+        {
+          BinaryFormatter bin = new BinaryFormatter();
+          bin.Serialize(stream, objToSerialize);
+        }
+      }
+      catch (IOException)
+      {
+      }
+    }
+
+    //Takes a .bin file and deserializes it into an object. 
+    //Returns the deserialized object.
+    public static T Load<T>(string filePath) where T : new()
+    {
+      T rez = new T();
+
+      try
+      {
+        using (Stream stream = File.Open(filePath, FileMode.Open))
+        {
+          BinaryFormatter bin = new BinaryFormatter();
+          rez = (T)bin.Deserialize(stream);
+        }
+      }
+      catch (IOException)
+      {
+      }
+
+      return rez;
+    }
+  }
+}
---
+++ WelcomForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkApp
+{
+  public partial class WelcomForm : Form
+  {
+    public WelcomForm()
+    {
+      InitializeComponent();
+    }
+
+    private void Greetings_lbl_Click(object sender, EventArgs e)
+    {
+      Greetings_lbl.Text = "Secret Message!";
+    }
+
+    private void ServerModeBtn_Click(object sender, EventArgs e)
+    {
+      Greetings_lbl.Text = "SERVER NOW RUNNING";
+      ServerModeBtn.Enabled = false;
+      ClientModeBtn.Enabled = false;
+      Server server = new Server();
+
+    }
+
+    private void ClientModeBtn_Click(object sender, EventArgs e)
+    {
+      ClientForm client = new ClientForm();
+      client.ShowDialog(this);
+      client.Dispose();
+    }
+  }
+}
